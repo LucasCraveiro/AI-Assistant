d3f355314572d6341df89e4e1032d186de1f39e7
Lucas Craveiro <lucashcraveiro@gmail.com>
2023-10-26 10:41:01 -0700

    Add Lazy Loading to blocks

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ inputElement.addEventListener("keydown", function (event) {
     imageWrapper.classList.remove("show");
   }
 });
+
+const handleIntersection = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+// Define a function to handle the intersection
+
+// Create an Intersection Observer
+const observer = new IntersectionObserver(handleIntersection);
+
+// Get all the sections you want to lazy load
+const sections = document.querySelectorAll(".lazy-section");
+
+// Observe each section
+sections.forEach((section) => {
+  observer.observe(section);
+});
